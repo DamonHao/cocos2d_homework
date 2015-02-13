474509ea768e308d25c24f2c1de3e3da8a7b11d7
DamonHao <haolinknight@gmail.com>
2015-02-14 00:05:13 +0800

    add physics between leading and stone

--- CocosLuaGame/src/AnimationLayer.lua
+++ CocosLuaGame/src/AnimationLayer.lua
@@ -5,16 +5,22 @@ local AnimationLayer = class("AnimationLayer", function()
 	return cc.Layer:create()
 end)
 
-local TAG_FOWARD_ACTION = 1
+local TAG_FORWARD_ACTION = 1
 local TAG_BACKWARD_ACTION = 2
-local TAG_MAP = 3
+local TAG_JUMP_FORWARD_ACTION = 3
+local TAG_JUMP_BACKWARD_ACTION = 4
+local TAG_JUMP_ACTION = 5
+local TAG_MAP = 103
+local TAG_LEADING_ROLE = 104
+
 local KEY_W = cc.KeyCode.KEY_W
 local KEY_D = cc.KeyCode.KEY_D
 local KEY_S = cc.KeyCode.KEY_S
 local KEY_A = cc.KeyCode.KEY_A
 local KEY_J = cc.KeyCode.KEY_J
 local KEY_K = cc.KeyCode.KEY_K
-local JUMP_HEIGHT = 120
+local JUMP_HEIGHT = 180
+local ALL_BIT_ONE = -1 -- 0xFFFFFFFF will overflow and set as -2147483648 = 0x80000000
 
 
 
@@ -38,6 +44,7 @@ function AnimationLayer:ctor()
     self.pressedDirectionKey = {[cc.KeyCode.KEY_W] = false, [cc.KeyCode.KEY_D] = false,
                            [cc.KeyCode.KEY_S] = false, [cc.KeyCode.KEY_A] = false}
     self.directionKeyNum = 0
+    self.isLeadingRoleOnTheGround = true
 end
 
 function AnimationLayer:onEnter()
@@ -49,15 +56,21 @@ function AnimationLayer:onEnter()
     nodeForWorldEdge:setPhysicsBody(cc.PhysicsBody:createEdgeBox(cc.size(self.visibleSize.width, self.visibleSize.height)))
     nodeForWorldEdge:setPosition(self.visibleSize.width/2, self.visibleSize.height/2)
     nodeForWorldEdge:getPhysicsBody():setDynamic(false)
+    nodeForWorldEdge:getPhysicsBody():setContactTestBitmask(ALL_BIT_ONE)
+    nodeForWorldEdge:setTag(TAG_MAP)
+    
 --    nodeForWorldEdge:getPhysicsBody():setCollisionBitmask(0x1)
     self:addChild(nodeForWorldEdge)
     
---    local stone = cc.Sprite:create("maps/stone.png")
---    stone:setPosition(self.visibleSize.width/2, 100)
---    stone:setPhysicsBody(cc.PhysicsBody:createBox(stone:getContentSize()))
---    stone:getPhysicsBody():setDynamic(false)
---    stone:getPhysicsBody():setTag(TAG_MAP)
---    self:addChild(stone)   
+    local stone = cc.Sprite:create("maps/stone.png")
+    stone:setPosition(self.visibleSize.width/2, 120)
+    stone:setPhysicsBody(cc.PhysicsBody:createBox(stone:getContentSize()))
+    stone:getPhysicsBody():setDynamic(false)
+    stone:getPhysicsBody():setContactTestBitmask(ALL_BIT_ONE)
+    stone:setTag(TAG_MAP)
+    cclog("stonde category:%d", stone:getPhysicsBody():getCategoryBitmask())
+    cclog("stonde contact test:%d", stone:getPhysicsBody():getContactTestBitmask())
+    self:addChild(stone)   
     
     local offsetUnit = 80
     local cache = cc.Director:getInstance():getTextureCache()
@@ -85,10 +98,15 @@ function AnimationLayer:onEnter()
 --    leadingRole:setAnchorPoint(0, 0)
 --    leadingRole:setPosition(0 , 0)
     leadingRole:setPosition(self.visibleSize.width/5, self.visibleSize.height/2)
-    leadingRole:setPhysicsBody(cc.PhysicsBody:createBox(leadingRole:getContentSize()))
+    leadingRole:setPhysicsBody(cc.PhysicsBody:createBox(cc.size(
+                leadingRole:getContentSize().width-15,leadingRole:getContentSize().height)))
+    leadingRole:getPhysicsBody():setRotationEnable(false)
     leadingRole:getPhysicsBody():setCategoryBitmask(1)
---    leadingRole:getPhysicsBody():setContactTestBitmask(1)
+    leadingRole:getPhysicsBody():setContactTestBitmask(1)
     leadingRole:getPhysicsBody():setCollisionBitmask(1)
+    leadingRole:setTag(TAG_LEADING_ROLE)
+    cclog("leadingRole category:%d", leadingRole:getPhysicsBody():getCategoryBitmask())
+    cclog("leadingRole contact test", leadingRole:getPhysicsBody():getContactTestBitmask())
     self:addChild(leadingRole)
     
     local testSprite = cc.Sprite:createWithSpriteFrame(forwardFrames[1]) 
@@ -97,8 +115,8 @@ function AnimationLayer:onEnter()
     testSprite:setPhysicsBody(cc.PhysicsBody:createBox(cc.size( offsetUnit , offsetUnit)))
 --    testSprite:getPhysicsBody():setContactTestBitmask(1)
     testSprite:getPhysicsBody():setCategoryBitmask(1)
-    testSprite:getPhysicsBody():setCollisionBitmask(2)
-    self:addChild(testSprite)
+    testSprite:getPhysicsBody():setCollisionBitmask(1)
+--    self:addChild(testSprite)
     
     -- keyboard event
     local function onKeyPressed(keyCode, event)
@@ -109,15 +127,15 @@ function AnimationLayer:onEnter()
             pressedKey[keyCode] = true 
             self.directionKeyNum = self.directionKeyNum + 1
         end
-        if keyCode == KEY_D then -- foward move
+        if keyCode == KEY_D and self.isLeadingRoleOnTheGround then -- foward move
             targetSprite:setFlippedX(false)
             local animation = cc.AnimationCache:getInstance():getAnimation("foward_walk")
             local walkAction = cc.Animate:create(animation)
             local moveBy = cc.MoveBy:create(5, cc.p(self.visibleSize.width, 0))
             local forward = cc.Spawn:create(walkAction, moveBy)
-            forward:setTag(TAG_FOWARD_ACTION)
+            forward:setTag(TAG_FORWARD_ACTION)
             targetSprite:runAction(forward)
-        elseif keyCode == KEY_A then -- backward move
+        elseif keyCode == KEY_A and self.isLeadingRoleOnTheGround then -- backward move
             targetSprite:setFlippedX(true)
             local animation = cc.AnimationCache:getInstance():getAnimation("foward_walk")
             local walkAction = cc.Animate:create(animation)
@@ -125,15 +143,23 @@ function AnimationLayer:onEnter()
             local backward = cc.Spawn:create(walkAction, moveBy)
             backward:setTag(TAG_BACKWARD_ACTION)
             targetSprite:runAction(backward)
-        elseif keyCode == KEY_J then -- jump
+        elseif keyCode == KEY_J and self.isLeadingRoleOnTheGround then -- jump
             if pressedKey[KEY_D] and pressedKey[KEY_A] then
-                targetSprite:runAction(cc.JumpBy:create(0.5, cc.p(0, 0), JUMP_HEIGHT, 1))	
+                local jump = cc.JumpBy:create(0.5, cc.p(0, 0), JUMP_HEIGHT, 1)
+                jump:setTag(TAG_JUMP_ACTION)
+                targetSprite:runAction(jump)	
             elseif pressedKey[KEY_D] then
-                targetSprite:runAction(cc.JumpBy:create(0.5, cc.p(80, 0), JUMP_HEIGHT, 1))
+                local jumpForward = cc.JumpBy:create(0.5, cc.p(100, 0), JUMP_HEIGHT, 1)
+                jumpForward:setTag(TAG_JUMP_FORWARD_ACTION)
+                targetSprite:runAction(jumpForward)
             elseif pressedKey[KEY_A] then
-                targetSprite:runAction(cc.JumpBy:create(0.5, cc.p(-80, 0), JUMP_HEIGHT, 1))
+                local jumpBackward = cc.JumpBy:create(0.5, cc.p(-100, 0), JUMP_HEIGHT, 1)
+                jumpBackward:setTag(TAG_JUMP_BACKWARD_ACTION)
+                targetSprite:runAction(jumpBackward)
             else
-                targetSprite:runAction(cc.JumpBy:create(0.5, cc.p(0, 0), JUMP_HEIGHT, 1))
+                local jump = cc.JumpBy:create(0.5, cc.p(0, 0), JUMP_HEIGHT, 1)
+                jump:setTag(TAG_JUMP_ACTION)
+                targetSprite:runAction(jump)
             end
         elseif keyCode == KEY_K then -- attack
             local attack = cc.Sprite:createWithSpriteFrame(
@@ -168,7 +194,7 @@ function AnimationLayer:onEnter()
                     attackPosi.x = lead_x
                     attackPosi.y = lead_y + leadSize.height/2 + attack:getContentSize().height/2
                     moveBy = cc.MoveBy:create(2, cc.p(0, self.visibleSize.height))
-                elseif pressedKey[KEY_S] then --down
+            elseif pressedKey[KEY_S] then --down
                     attack:setRotation(90)
                     attackPosi.x = lead_x
                     attackPosi.y = lead_y - leadSize.height/2 - attack:getContentSize().height/2
@@ -217,9 +243,9 @@ function AnimationLayer:onEnter()
             local function cleanupAttack()
                 self:removeChild(attack, true)
             end
-            local callfunc = cc.CallFunc:create(cleanupAttack)
-            attack:runAction(cc.Sequence:create(moveBy, callfunc))
-            attack:setPosition(attackPosi)
+        local callfunc = cc.CallFunc:create(cleanupAttack)
+        attack:runAction(cc.Sequence:create(moveBy, callfunc))
+        attack:setPosition(attackPosi)
             self:addChild(attack)
             cclog("Aniamtion layer child nums: %d", self:getChildrenCount())
         end
@@ -230,7 +256,7 @@ function AnimationLayer:onEnter()
         local targetSprite = event:getCurrentTarget()
         if pressedKey[keyCode] then
             if keyCode == cc.KeyCode.KEY_D then
-                targetSprite:stopActionByTag(TAG_FOWARD_ACTION)
+                targetSprite:stopActionByTag(TAG_FORWARD_ACTION)
             elseif keyCode == cc.KeyCode.KEY_A then
                 targetSprite:stopActionByTag(TAG_BACKWARD_ACTION)
             end
@@ -247,19 +273,86 @@ function AnimationLayer:onEnter()
     
     -- contact event
     local function onContactBegin(contact)
-        local a = contact:getShapeA():getBody()
-        local b = contact:getShapeB():getBody()
-        print("contact") 
---        if a:getTag() == TAG_MAP or b:getTag() == TAG_MAP then
---            print("contact")    
---        end
+        local a = contact:getShapeA():getBody():getNode()
+        local b = contact:getShapeB():getBody():getNode()
+        cclog("contact in") 
+        if a:getTag() == TAG_MAP or b:getTag() == TAG_MAP then
+            local map, dynamicSprite
+            local isSwapped = false
+            if a:getTag() == TAG_MAP then
+                map = a 
+                dynamicSprite = b    
+            else
+                map = b 
+                dynamicSprite = a
+                isSwapped = true  
+            end
+            if dynamicSprite:getTag() == TAG_LEADING_ROLE then
+                local contactNormal = contact:getContactData().normal
+                local velocity = dynamicSprite:getPhysicsBody():getVelocity()
+                cclog("velocity before contact %f, %f", velocity.x, velocity.y)
+                if contactNormal.y >= 0.1 or contactNormal.y <= -0.1 then
+                    if isSwapped then -- set the normal emit from map
+                        contactNormal.y = -1 * contactNormal.y 
+                    end
+                    cclog("onContactBegin current normal y: %f",  contactNormal.y)
+                    if contactNormal.y >= 0.1 then
+                        self.isLeadingRoleOnTheGround = true
+                    end
+                    dynamicSprite:stopActionByTag(TAG_JUMP_FORWARD_ACTION)
+                    dynamicSprite:stopActionByTag(TAG_JUMP_BACKWARD_ACTION)
+                    dynamicSprite:stopActionByTag(TAG_JUMP_ACTION)
+                    dynamicSprite:getPhysicsBody():setVelocity(cc.p(velocity.x, 0))
+                else
+                    dynamicSprite:getPhysicsBody():setVelocity(cc.p(0, velocity.y))
+                    dynamicSprite:stopActionByTag(TAG_JUMP_FORWARD_ACTION)
+                    dynamicSprite:stopActionByTag(TAG_JUMP_BACKWARD_ACTION)
+                end
+                velocity = dynamicSprite:getPhysicsBody():getVelocity()
+                cclog("velocity after contact %f, %f", velocity.x, velocity.y)
+                
+            end
+--            print("touch")
+--            local contactPoint = contact:getContactData().points
+        end
     	return true
     end
     
+    local function onContactEnd(contact)
+        local a = contact:getShapeA():getBody():getNode()
+        local b = contact:getShapeB():getBody():getNode()
+        if a:getTag() == TAG_MAP or b:getTag() == TAG_MAP then
+            local map, dynamicSprite
+            local isSwapped = false
+            cclog("contact end")
+            if a:getTag() == TAG_MAP then
+                map = a 
+                dynamicSprite = b    
+            else
+                map = b 
+                dynamicSprite = a
+                isSwapped = true  
+            end
+            if dynamicSprite:getTag() == TAG_LEADING_ROLE then
+                local contactNormal = contact:getContactData().normal
+                if contactNormal.y >= 0.1 or contactNormal.y <= -0.1 then
+                    if isSwapped then -- set the normal emit from map
+                        contactNormal.y = -1 * contactNormal.y 
+                    end
+                    cclog("onContactEnd current normal y: %f",  contactNormal.y)
+                    if contactNormal.y >= 0.1 then
+                        self.isLeadingRoleOnTheGround = false
+                    end
+                end
+            end
+        end
+        return true
+    end
     local contactListener = cc.EventListenerPhysicsContact:create()
 --    local contactListener = cc.EventListenerPhysicsContactWithBodies:create(leadingRole:getPhysicsBody(), 
 --                            testSprite:getPhysicsBody())
     contactListener:registerScriptHandler(onContactBegin, cc.Handler.EVENT_PHYSICS_CONTACT_BEGIN)
+    contactListener:registerScriptHandler(onContactEnd, cc.Handler.EVENT_PHYSICS_CONTACT_SEPERATE)
     eventDispatcher:addEventListenerWithSceneGraphPriority(contactListener, self)
     
 end
